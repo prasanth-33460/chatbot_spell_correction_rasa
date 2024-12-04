c6da662f5a10f2ca706e792db57269d2f7ce5dbb
Prasanth Thiagarajan <prasanth33460@gmail.com>
2024-12-05 00:07:31 +0530

    dandanaka danakunaka

--- actions/actions.py
+++ actions/actions.py
@@ -16,3 +16,16 @@ class ActionCorrectSpelling(Action):
             dispatcher.utter_message(text=f"Corrected text: {corrected_text_str}")
             return [SlotSet("corrected_text", corrected_text_str)]
         return []
+    
+class ActionTrackChestPain(Action):
+    def name(self) -> str:
+        return "action_track_chest_pain"
+
+    def run(self, dispatcher, tracker, domain):
+        chest_pain_responded = tracker.get_slot("chest_pain_responded")
+        if chest_pain_responded is False:
+            dispatcher.utter_message(response="utter_ask_for_medical_help")
+            return [SlotSet("chest_pain_responded", True)]  
+        else:
+            dispatcher.utter_message(response="utter_follow_up_action")
+            return []  
